74d0d2f475879a5b767fa67e71be0978d2849f56
Doug McGill <doug@brainpowerstudio.com>
2015-09-23 09:31:20 -0400

    fixed untracked files

--- BouncerBall/Intermediate/Build/Win64/UE4Editor/Development/BouncerBall/BouncerBall.h
+++ BouncerBall/Intermediate/Build/Win64/UE4Editor/Development/BouncerBall/BouncerBall.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_BOUNCERBALL_H__
 #define __AUTO_BOUNCERBALL_H__
-//Last Write: 2015-09-21 2:19:58 PM +00:00
-#include "C:\Users\Jarrett\Documents\Unreal Projects\Bouncer-Ball\BouncerBall\Source\BouncerBall\BouncerBall.h"
+//Last Write: 9/21/2015 2:47:23 AM +00:00
+#include "C:\Users\Doug\Documents\Unreal Projects\Bouncer-Ball\BouncerBall\Source\BouncerBall\BouncerBall.h"
 #endif//__AUTO_BOUNCERBALL_H__
--- BouncerBall/Intermediate/Build/Win64/UE4Editor/Development/BouncerBall/PCH.BouncerBall.h.cpp
+++ BouncerBall/Intermediate/Build/Win64/UE4Editor/Development/BouncerBall/PCH.BouncerBall.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\Jarrett\Documents\Unreal Projects\Bouncer-Ball\BouncerBall\Source\BouncerBall\BouncerBall.h"
+#include "C:\Users\Doug\Documents\Unreal Projects\Bouncer-Ball\BouncerBall\Source\BouncerBall\BouncerBall.h"
