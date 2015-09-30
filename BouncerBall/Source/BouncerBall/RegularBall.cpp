c58f1ad3ffbbef764a5d03111dddd20a4c583796
Doug McGill <doug@brainpowerstudio.com>
2015-09-30 10:10:28 -0400

    Added regular ball and updated Ball

---
+++ BouncerBall/Source/BouncerBall/RegularBall.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BouncerBall.h"
+#include "RegularBall.h"
+
+ARegularBall::ARegularBall()
+{
+	
+}
+void ARegularBall::Move()
+{
+
+}
+
