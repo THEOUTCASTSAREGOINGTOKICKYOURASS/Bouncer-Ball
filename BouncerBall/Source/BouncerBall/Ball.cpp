9f52f3a386924499f26b9a296f51788850bec5f3
Doug McGill <doug@brainpowerstudio.com>
2015-09-30 10:20:14 -0400

    fixed merge issue

--- BouncerBall/Source/BouncerBall/Ball.cpp
+++ BouncerBall/Source/BouncerBall/Ball.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BouncerBall.h"
@@ -31,41 +31,4 @@ void ABall::GetTarget()
 void ABall::Move()
 {
 
-=======
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BouncerBall.h"
-#include "Ball.h"
-
-
-// Sets default values
-ABall::ABall()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void ABall::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ABall::Tick( float DeltaTime )
-{
-	Super::Tick( DeltaTime );
-
-}
-
-void ABall::Move()
-{
-
-}
-void ABall::GetTarget()
-{
-
->>>>>>> origin/master
 }
