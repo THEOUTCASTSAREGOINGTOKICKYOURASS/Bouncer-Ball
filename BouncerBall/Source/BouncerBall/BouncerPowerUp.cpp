514bbdf4fc1fed8df958471d78b760a6d7706f51
Douglas McGill <dougmcgill08@gmail.com>
2015-11-04 10:41:16 -0500

    created base class for powerup with a single virtual to be overriden

---
+++ BouncerBall/Source/BouncerBall/BouncerPowerUp.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BouncerBall.h"
+#include "BouncerPowerUp.h"
+
+BouncerPowerUp::BouncerPowerUp()
+{
+}
+
+BouncerPowerUp::~BouncerPowerUp()
+{
+}
