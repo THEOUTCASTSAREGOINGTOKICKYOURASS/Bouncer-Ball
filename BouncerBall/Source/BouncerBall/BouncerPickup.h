83550b6bff054fc472c8794270c21be77aeb7b8f
Douglas McGill <dougmcgill08@gmail.com>
2015-11-04 09:53:51 -0500

    Added Base pickup class and tested in game

---
+++ BouncerBall/Source/BouncerBall/BouncerPickup.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "BouncerPickup.generated.h"
+
+UCLASS()
+class BOUNCERBALL_API ABouncerPickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABouncerPickup();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		USphereComponent* Collider;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* Mesh;
+
+	UFUNCTION()
+		virtual void OnBeginOverlap(AActor* OtherActor);
+	
+};
